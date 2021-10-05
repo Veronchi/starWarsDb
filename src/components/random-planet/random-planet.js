25027aaa198736feb18c07923b7fcd41703970f5
Veronchi <zhigunova1995@gmail.com>
2021-10-05 14:57:25 +0300

    add getting data from api

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,34 +1,60 @@
 import React, { Component } from "react";
+import SwapiService from '../../services/swapi-service'
 
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
 
+  swapiService = new SwapiService();
+
   state = {
     name: null,
+    id: null,
     population: null,
     rotationPeriod: null,
     diameter: null,
   };
 
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 25)+ 2;
+    this.swapiService
+      .getPlanet(id)
+      .then((planet) => {
+        this.setState({
+          id,
+          name: planet.name,
+          population: planet.population,
+          rotationPeriod: planet.rotation_period,
+          diameter: planet.diameter,
+        });
+      });
+  };
+
   render() {
+    const {name, id, population, rotationPeriod, diameter} = this.state;
+
     return (
       <div className='random-planet text-white bg-dark mb-3'>
-        <img className='planet-image' src='https://starwars-visualguide.com/assets/img/planets/5.jpg' alt='planet' />
+        <img className='planet-image' src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt='planet' />
         <div className='planet-info'>
-          <h2 className='planet-name'>Planet Name</h2>
+          <h2 className='planet-name'>{name}</h2>
           <ul className='planet-info-list list-group-flush'>
             <li className='list-group-item'>
               <span className='term'>Population</span>
-              <span>1232</span>
+              <span>{population}</span>
             </li>
             <li className='list-group-item'>
               <span className='term'>Rotation Period</span>
-              <span>111</span>
+              <span>{rotationPeriod}</span>
             </li>
             <li className='list-group-item'>
               <span className='term'>Diameter</span>
-              <span>1</span>
+              <span>{diameter}</span>
             </li>
           </ul>
         </div>
